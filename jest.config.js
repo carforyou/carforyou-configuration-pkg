c21aa2679d306abf7381fa4396666a9ca255bbd6
Christian Felder (masone) <ema@rh-productions.ch>
2022-01-13 11:24:37 +0100

    fix: typescript for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   globals: {},
   testPathIgnorePatterns: ["<rootDir>/pkg/"],
-  moduleFileExtensions: ["js", "json", "node"],
-  testMatch: ["**/__tests__/**/*.test.js"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  testMatch: ["**/__tests__/**/*.test.(t|j)s"],
+  preset: "ts-jest",
 }
